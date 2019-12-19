be0d4f6ed85b077931fe04c51c428959c14fa93b
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2019-12-20 08:17:54 +0900

    クラス毎にコードを分離 #8　数学の関数と定数を分離

---
+++ raytracing_test/inline_math.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define PI 3.14159265359f
+#define PI2 6.28318530718f
+#define RECIP_PI 0.31830988618f
+#define RECIP_PI2 0.15915494f
+#define LOG2 1.442695f
+#define EPSILON 1e-6f
+#define GAMMA_FACTOR 2.2f
+
+#define MAX_DEPTH 50
+
+#include <random>
+inline double drand48() {
+	return float(((double)(rand()) / (RAND_MAX))); /* RAND_MAX = 32767 */
+}
+
+inline float pow2(float x) { return x * x; }
+inline float pow3(float x) { return x * x * x; }
+inline float pow4(float x) { return x * x * x * x; }
+inline float pow5(float x) { return x * x * x * x * x; }
+inline float clamp(float x, float a, float b) { return x < a ? a : x > b ? b : x; }
+inline float saturate(float x) { return x < 0.f ? 0.f : x > 1.f ? 1.f : x; }
+inline float recip(float x) { return 1.f / x; }
+inline float mix(float a, float b, float t) { return a * (1.f - t) + b * t; /* return a + (b-a) * t; */ }
+inline float step(float edge, float x) { return (x < edge) ? 0.f : 1.f; }
+inline float smoothstep(float a, float b, float t) { if (a >= b) return 0.f; float x = saturate((t - a) / (b - a)); return x * x * (3.f - 2.f * t); }
+inline float radians(float deg) { return (deg / 180.f) * PI; }
+inline float degrees(float rad) { return (rad / PI) * 180.f; }
