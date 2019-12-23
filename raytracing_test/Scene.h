05fe89fc05c7835dae571006002156e7975bd62a
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2019-12-23 23:15:57 +0900

    クラス毎にコードを分離 #8　Scene クラスの backgroundSky 関数が未使用だったので削除

--- raytracing_test/Scene.h
+++ raytracing_test/Scene.h
@@ -16,15 +16,7 @@ namespace rayt {
 			, m_samples(samples) { }
 
 		void build(float r_param, float g_param, float b_param, float refractive_param);
-
 		vec3 color(const rayt::Ray& r, const Shape* world, int depth) const;
-
-		vec3 backgroundSky(const vec3& d) const {
-			vec3 v = normalize(d);
-			float t = 0.5f * (v.getY() + 1.0f);
-			return lerp(t, vec3(1), vec3(0.5f, 0.7f, 1.0f));
-		}
-
 		void render(int threadNum, int numThread, Vector3 image[], const Vector3& rgb_param, const float refractive_param);
 
 	private:
