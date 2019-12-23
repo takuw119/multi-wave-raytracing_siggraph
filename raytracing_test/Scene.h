afb3266d0338523bf478094017bcb6b7f931bcbb
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2019-12-24 00:06:11 +0900

    クラス毎にコードを分離 #8　関数を private に変更

--- raytracing_test/Scene.h
+++ raytracing_test/Scene.h
@@ -12,12 +12,12 @@ namespace rayt {
 	public:
 		Scene(int width, int height, int samples);
 		~Scene();
-
 		void build(float r_param, float g_param, float b_param, float refractive_param);
-		vec3 color(const rayt::Ray& r, const Shape* world, int depth) const;
 		void render(int threadNum, int numThread, Vector3 image[], const Vector3& rgb_param, const float refractive_param);
 
 	private:
+		vec3 color(const rayt::Ray& r, const Shape* world, int depth) const;
+
 		std::unique_ptr<Camera> m_camera;
 		std::unique_ptr<Image> m_image;
 		std::unique_ptr<Shape> m_world;
