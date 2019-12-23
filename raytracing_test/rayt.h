9e26a0a2174a98fa000e5f3c384a0a5f33b418aa
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2019-12-23 21:19:32 +0900

    クラス毎にコードを分離 #8　Ray クラスを分離

--- raytracing_test/rayt.h
+++ raytracing_test/rayt.h
@@ -9,28 +9,11 @@
 #define NUM_THREAD 24
 
 #include "inline_math.h"
+#include "Ray.h"
 
 using namespace std;
 
 namespace rayt {
-	class Ray {
-	public:
-		Ray() {}
-		Ray(const vec3& o, const vec3& dir)
-			: m_origin(o)
-			, m_direction(dir) { }
-
-		const vec3& origin() const { return m_origin; }
-		const vec3& direction() const { return m_direction; }
-		vec3 at(float t) const { return m_origin + t * m_direction; }
-
-	private:
-		vec3 m_origin; // Start Point
-		vec3 m_direction; // Direction (Denormalized)
-	};
-
-	//--------------------------------------------------------------------------
-
 	class Camera {
 	public:
 		Camera() {}
